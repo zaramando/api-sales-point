7e60c90f3e0fcaaf113c0d09bebdd743424a5beb
Armando Zarate Mallarino <azarate@sura.com.co>
2019-09-11 10:36:31 -0500

    cambio JWT HS256

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -6,8 +6,8 @@ class AuthenticationController < ApplicationController
    
       if command.success?
         render json: { auth_token: command.result }
-      else
-        render json: { error: command.errors }, status: :unauthorized
+      else        
+        error!(command.errors, :unauthorized)
       end
     end
 end
--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,7 +2,7 @@ class JsonWebToken
     class << self
       def encode(payload, exp = 24.hours.from_now)
         payload[:exp] = exp.to_i
-        JWT.encode(payload, Rails.application.secrets.secret_key_base)
+        JWT.encode(payload, Rails.application.secrets.secret_key_base, 'HS256')
       end
    
       def decode(token)
