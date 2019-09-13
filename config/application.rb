2e5b3cb28a1647d3878c860ceb9b345beb7e8b2d
Armando Zarate Mallarino <azarate@sura.com.co>
2019-09-12 21:16:15 -0500

    cors

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module ApiApp
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :head :options]
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :head, :options]
       end
     end
     # Only loads a smaller set of middleware suitable for API only apps.
