3e4aa3d9b27a85d67c11ad79dda160e1e4d9400e
Armando Zarate Mallarino <azarate@sura.com.co>
2019-09-12 20:40:23 -0500

    se elimina Rack::cors de aplication.rb

--- config/application.rb
+++ config/application.rb
@@ -24,17 +24,16 @@ module ApiApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.autoload_paths << Rails.root.join('lib')
-    config.middleware.insert_before Warden::Manager, Rack::Cors
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-         origins '*'
-         resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
-       end
-    end
+   # config.middleware.insert_before 0, Rack::Cors do
+    #  allow do
+     #    origins '*'
+      #   resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+       #end
+    #end
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
