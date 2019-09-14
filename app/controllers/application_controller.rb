d6b6dc86d5286a7d75d4382f499c001d2658668a
Armando Zarate Mallarino <azarate@sura.com.co>
2019-09-14 14:29:55 -0500

    vuelta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
     before_action :set_jbuilder_defaults
-    before_action :xhr_options_request 
-    before_action :cors_set_access_control_headers     
+    before_action :cors_set_access_control_headers 
+    before_action :xhr_options_request    
     before_action :authenticate_request         
     attr_reader :current_user
 
