ec4e8a747581f3e6a004cf55dee1501217c862ac
Armando Zarate Mallarino <azarate@sura.com.co>
2019-09-14 00:54:47 -0500

    Credentials true

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::API
         headers['Access-Control-Allow-Origin'] = "*"
         headers['Access-Control-Allow-Methods']= "POST,GET,PUT,DELETE,OPTIONS"
         headers['Access-Control-Allow-Headers'] = 'Origin,Content-Type,Accept,Authorization,Token'
+        headers['Access-Control-Allow-Credentials'] = true
     end    
 
 
