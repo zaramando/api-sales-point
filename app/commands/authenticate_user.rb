0a7701dbc70a9755a2709ee816db4a8896a10e5c
Armando Zarate Mallarino <azarate@sura.com.co>
2019-09-13 23:52:37 -0500

    add name and email at token

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -7,7 +7,7 @@ class AuthenticateUser
     end
   
     def call
-      JsonWebToken.encode(user_id: user.id) if user
+      JsonWebToken.encode(user_id: user.id, name: user.name, email: user.email) if user
     end
   
     private
