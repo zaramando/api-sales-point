f2e075a287a8d6f84b79d8db54b5316f2c10d886
Armando Zarate Mallarino <azarate@sura.com.co>
2019-09-14 00:23:40 -0500

    cors_set quit

--- app/commands/authorize_api_request.rb
+++ app/commands/authorize_api_request.rb
@@ -1,5 +1,4 @@
 class AuthorizeApiRequest
-    after_action :cors_set_access_control_headers
     prepend SimpleCommand
   
     def initialize(headers = {})
@@ -9,12 +8,6 @@ class AuthorizeApiRequest
     def call
       user
     end
-
-    def cors_set_access_control_headers
-      headers['Access-Control-Allow-Origin'] = "*"
-      headers['Access-Control-Allow-Methods']= "POST,GET,PUT,DELETE,OPTIONS"
-      headers['Access-Control-Allow-Headers'] = 'Origin,Content-Type,Accept,Authorization,Token'
-    end  
   
     private
   
