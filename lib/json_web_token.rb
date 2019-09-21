acb39ea267d1d439e2e0216fbd804a7d6deecf3c
Armando Zarate Mallarino <azarate@sura.com.co>
2019-09-21 01:44:07 -0500

    token firmado test

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,13 +2,13 @@ class JsonWebToken
     class << self
       def encode(payload, exp = 24.hours.from_now)
         payload[:exp] = exp.to_i
-        #JWT.encode(payload, Rails.application.secrets.secret_key_base)
-        JWT.encode(payload, nil, 'none')
+        JWT.encode(payload, Rails.application.secrets.secret_key_base)
+        #JWT.encode(payload, nil, 'none')
       end
    
       def decode(token)
-        #body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
-        body = JWT.decode(token, nil, false)[0]
+        body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+        #body = JWT.decode(token, nil, false)[0]
         HashWithIndifferentAccess.new body
       rescue
         nil
